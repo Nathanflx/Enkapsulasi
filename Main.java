2da1049a6a95ba1171749dbfaaed943b8ebac6fc
Nathanflx <nathanaelfh78@gmail.com>
2024-02-01 09:15:36 +0700

    first commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+package latihan_modifier;
+
+class main extends person{
+    public static void main(String[] args) {
+
+    person mPerson = new person();
+    mPerson.setName("Ampun");
+
+    System.out.println("Dj " + mPerson.getName());
+
+    }
+}
---
+++ person.java
@@ -0,0 +1,22 @@
+package latihan_modifier;
+
+public class person {
+    
+class person {
+    public String name;
+    private String username;
+    private String password;
+
+        public void setName(String name){
+            this.name = name;
+        }
+    
+        public String getName(){
+            return this.name;
+        }
+
+        public person (){
+            System.out.println("eksekusi method constructor...");
+        }
+}
+}
